7bbae3ae644bca924bab47340d957e2c830de54d
iran <iran.csousa21@gmail.com>
2026-05-03 14:23:59 -0300

    Codigo Finalizado

---
+++ script.js
@@ -0,0 +1,25 @@
+let nomeHeroi = "K4M1K4Z3"
+let xpHeroi = 1000
+let rank = ""
+
+console.log(`O Heroi chamado ${nomeHeroi} passui ${xpHeroi} pontos de XP`)
+
+if(xpHeroi <= 1000){
+    rank = "Ferro"
+}else if(xpHeroi < 2000){
+    rank = "Bronze"
+}else if(xpHeroi < 5000){
+    rank = "Prata"
+}else if(xpHeroi < 7000){
+    rank = "Ouro"
+}else if(xpHeroi < 8000){
+    rank = "Platina"
+}else if(xpHeroi < 9000){
+    rank = "Ascendente"
+}else if(xpHeroi < 10000){
+    rank = "Imortal"
+}else{
+    rank = "Radiante"
+}
+
+console.log(`O Heroi ${nomeHeroi} e rank ${rank}`)
